b0cb4481cbb8c842c6eea7af3689c666b7c28f0e
Sai Santosh <saibhalerao744@gmail.com>
2026-02-05 12:07:57 +0530

    Built AI-powered real-time human surveillance system

---
+++ human_detection.py
@@ -0,0 +1,81 @@
+from ultralytics import YOLO
+import cv2
+import math
+import time
+# Initialize the camera and set the image resolution
+cap = cv2.VideoCapture(0)
+cap.set(3, 720)
+cap.set(4, 720)
+
+# Load our model
+model = YOLO('yolo-Weights/yolov8n.pt')
+prev_time = 0
+# Define the classes we are going to detect
+classNames = [
+    'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 
+    'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 
+    'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 
+    'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 
+    'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 
+    'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 
+    'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 
+    'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 
+    'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+
+# Capture Loop
+while True:
+
+    success, img = cap.read()
+    current_time = time.time()
+    fps = 1 / (current_time - prev_time) if prev_time != 0 else 0
+    prev_time = current_time
+
+    cv2.putText(img, f"FPS: {int(fps)}", (20, 40),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    # Read the image from the camera
+    results = model(img, stream=True)  # Send the image to YOLO for detection
+
+    # Loop over detected objects
+    for r in results:
+        boxes = r.boxes  # Get bounding boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]  # Get bounding box coordinates
+            x1, y1, x2, y2 = map(int, [x1, y1, x2, y2])  # Convert to int
+            
+            # Detect the class name of the object
+            cls = int(box.cls[0])
+            classname = classNames[cls]
+            if classname != 'person':
+                continue
+            # Calculate the confidence of the detected object
+            confidence = math.ceil(box.conf[0] * 100)
+            if confidence < 60:
+                continue
+            # print(f'{classname}: {confidence}%')
+
+
+            color = (0, 0, 255)
+
+            # Draw the rectangle on the image
+            cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
+
+            # Write the class name and confidence on the image
+            org = (x1, y1 - 10)  # Position the text above the bounding box
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(img, f"{classname} {confidence:.2f}%", org, font, 0.5, color, 2)
+            if classname == "person":
+                cv2.putText(img, "ALERT: HUMAN DETECTED",
+                            (20, 100),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1, (0, 0, 255), 3)
+
+    # Create a window to display the image
+    cv2.imshow("Webcam", img)
+
+    # Exit loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
